5c265e8d08f16f243411e70bf026af8385bf0ba2
Anup Datta <anupdutta96@gmail.com>
2025-01-11 01:10:44 +0530

    added comment to test webhook

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -4,6 +4,8 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
+//added comment to chech test discord webhook 
+
 export const prisma = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
