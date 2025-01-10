9875bb4caa4f0916b0b825e53984efc0426827c7
Yashwanth <dyashwanth14011999@gmail.com>
2025-01-11 00:04:08 +0530

    refactor(http): remove testing code from index.ts

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -4,14 +4,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', async (req: Request, res: Response) => {
-    //testing prisma here
-    const response = await prisma.user.create({
-        data:{
-            email:"abc@gmail.com",
-            userName:"abc",
-            password:"pass123",
-        }
-    })
     res.status(200).send('Hello World!');
 });
 
