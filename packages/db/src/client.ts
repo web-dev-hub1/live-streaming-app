06380bc0003eab68513b198e1e73bfbd7254406f
Yashwanth <dyashwanth14011999@gmail.com>
2025-01-15 23:24:19 +0530

    refactor: optimize PrismaClient instantiation for global usage

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,5 +1,11 @@
+import { PrismaClient } from "@prisma/client";
 import dotenv from 'dotenv';
 dotenv.config({ path: '../../.env' });
 
-import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+//@ts-ignore
+export const prisma = global.prisma || new PrismaClient();
+//@ts-ignore
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
