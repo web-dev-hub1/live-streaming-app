fc6f4d5fdb257cba10f9668a8b914aa28cc19bc8
Yashwanth <dyashwanth14011999@gmail.com>
2025-01-15 23:43:11 +0530

    refactor: move PrismaClient global declaration to a separate file

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -2,10 +2,5 @@ import { PrismaClient } from "@prisma/client";
 import dotenv from 'dotenv';
 dotenv.config({ path: '../../.env' });
 
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
-//@ts-ignore
-export const prisma = global.prisma || new PrismaClient();
-//@ts-ignore
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+export const prisma = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
